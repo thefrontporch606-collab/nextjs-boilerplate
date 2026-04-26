a7d9379a516aa647c312520778855b1550ad3579
thefrontporch606-collab <thefrontporch606@gmail.com>
2026-04-26 07:07:21 -0400

    Update FloatingDonateButton.tsx

--- components/FloatingDonateButton.tsx
+++ components/FloatingDonateButton.tsx
@@ -1,1 +1,10 @@
-
+export default function FloatingDonateButton() {
+  return (
+    <a
+      href="/donate"
+      className="fixed bottom-6 right-6 z-50 rounded-full bg-blue-600 px-5 py-3 text-white shadow-lg hover:bg-blue-700"
+    >
+      Donate
+    </a>
+  );
+}
